96ef0378343715fdd0bfbac2ce2b4d7856101971
Yohana <hanneytewie@gmail.com>
2024-05-13 00:46:12 +0400

    contents and intro pages updated

--- src/homePage/Contents.java
+++ src/homePage/Contents.java
@@ -4,4 +4,6 @@ public class Contents {
     //this is all about the contents table
     //it tells the pages and the content
 
+    //adding update to the contents page
+
 }
--- src/homePage/IntroPage.java
+++ src/homePage/IntroPage.java
@@ -4,4 +4,6 @@ public class IntroPage {
     //Intro of the book
     //it tells highlight of the book
 
+    //adding contents to the intro page
+
 }
